4fa1c4ab5253d7e415cf4e56ef6fb10951e2adb9
ShivangeeSingh <51319226+ShivangeeSingh@users.noreply.github.com>
2019-06-10 11:29:39 -0400

    This code will calculate the age of the user

---
+++ age.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+username = input("Enter your name:")
+userage = int(input("Enter your age:"))
+year = str((2019 - userage)+95)
+
+print(username + "will be 95 in the year" + year)
+
